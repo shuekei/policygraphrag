93b91c5989cd1e7e1221a602ba4073d93e087b84
shuekei <92931877+shuekei@users.noreply.github.com>
2026-01-08 12:04:08 +0800

    Update rag_batch.py

--- rag_batch.py
+++ rag_batch.py
@@ -30,7 +30,7 @@ print("✅ Neo4j connected.")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-print("🤖 LLM model       :", "gemma3:12b")
+print("🤖 LLM model       :", "gpt-oss:120b")
 
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-Embedding-0.6B")
 model = AutoModel.from_pretrained("Qwen/Qwen3-Embedding-0.6B").to(device)
@@ -226,7 +226,7 @@ QUESTION:
 """
 
     payload = {
-        "model": "gemma3:12b",
+        "model": "gpt-oss:120b",
         "prompt": prompt,
         "stream": True
     }
@@ -448,6 +448,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
